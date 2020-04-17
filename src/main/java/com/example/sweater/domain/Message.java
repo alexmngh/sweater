2531dbfbf5e4e06a6c7fe0a02e8358ddf65a4af1
alex_man <alexmnsbs@gmail.com>
2020-04-17 15:11:08 +0300

    добавил проперти и класс для подключения к БД

---
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -0,0 +1,33 @@
+package com.example.sweater.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // аннотация говорит, что это сущность которую необходимо сохранить в БД
+public class Message {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)  //эта аннотация говорит чтобы spring сам генерил это поле
+    private Integer id; //аннотация @Id говорит, что поле id будет идентификаторм
+
+    private String text;
+
+    private String tag;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
